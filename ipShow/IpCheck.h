7890eb75aeed82ebfe1d97bb984726be94331ffe
lveyo <lveyoly@gmail.com>
2012-02-04 12:40:23 +0800

    first commit.

---
+++ ipShow/IpCheck.h
@@ -0,0 +1,13 @@
+//
+//  IpCheck.h
+//  ipShow
+//
+//  Created by Lv Yong on 12-2-4.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IpCheck : NSObject
+
+@end
---
+++ ipShow/IpCheck.m
@@ -0,0 +1,23 @@
+//
+//  IpCheck.m
+//  ipShow
+//
+//  Created by Lv Yong on 12-2-4.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "IpCheck.h"
+
+@implementation IpCheck
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
